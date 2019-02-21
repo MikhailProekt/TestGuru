4791cb32b393862b740deca060fe0418707d2118
MikhailProekt <proect24@mail.ru>
2019-02-21 16:37:10 +0300

    ActiveRecord_4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   def tests_by_level(level)
     Test.joins('join test_passages on test_passages.test_id = tests.id')
-    .where(tests: { level: level })
+    .where(tests: { level: level }, test_passages: { user_id: id } )
   end
 
 end
