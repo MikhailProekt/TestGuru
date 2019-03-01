10a1e055821f5278ceac094cfa49c381947b4b04
MikhailProekt <proect24@mail.ru>
2019-03-01 18:01:18 +0300

    testguru5ara_7

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,6 +8,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :author_id
 
   def self.select_by_category_title(category_title)
+    joins(:category)
     .where(categories: { title: category_title })
     .order(title: :desc)
     .pluck(:title)
