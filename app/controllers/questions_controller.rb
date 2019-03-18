5ec6c207a0d39f8b977755d60aa7ade4fa9c26da
MikhailProekt <proect24@mail.ru>
2019-03-18 15:09:58 +0300

    rails_controller_1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,26 @@
 class QuestionsController < ApplicationController
 
   before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy edit update]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def new
-
-  end
+  def new; end
 
   def index
     render inline: '<p>Questions:<br><%= @test.questions.pluck(:body) %></p>'
   end
 
   def create
-    question = Question.create(question_params)
-    render plain: "Question was created: " + question.inspect
+    @question = @test.questions.create!(question_params)
+    render plain: "Question was created: " + @question.inspect
   end
 
   def show
-    @question = Question.find(params[:id])
     render inline: '<p>Question:<br><%= @question.body %></p>'
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
     render inline: '<p>Question <%= @question.id %> was deleted</p>'
   end
@@ -34,8 +31,12 @@ class QuestionsController < ApplicationController
     @test = Test.find(params[:test_id])
   end
 
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
   def question_params
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   def rescue_with_question_not_found
