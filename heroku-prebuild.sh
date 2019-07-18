c86f4265c0e746e326394b34dbde5f15196ce3b0
MikhailProekt <proect24@mail.ru>
2019-07-18 15:06:31 +0300

    appdeploy10

--- heroku-prebuild.sh
+++
@@ -1,4 +0,0 @@
-if [ "$GITHUB_TOKEN" != "" ]; then
- echo "Detected GITHUB_TOKEN. Setting git config to use the security token" >&1
- git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf [removed_email]:
-fi
